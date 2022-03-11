e0fc2916c420a8807c49e58fcb73438c2429be54
Zach Stone <zachinjapan888@gmail.com>
2022-03-11 18:28:21 -0500

    try to add google adds using gastby-ssr.js

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,11 +2,10 @@ const React = require("react")
 
 const HeadComponents = [
   <script
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-
-    pub-2495439414515461"
-    crossOrigin="anonymous"
     async
-  />,
+    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2495439414515461"
+    crossorigin="anonymous"
+  ></script>,
 ]
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
